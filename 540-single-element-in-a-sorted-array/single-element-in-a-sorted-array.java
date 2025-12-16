6063baeedff7dcdc356938d3aaa5fcc105026dee
Rounak Raj <rounak00003@gmail.com>
2025-12-16 23:30:57 +0530

    Time: 1 ms (25.49%) | Memory: 52.8 MB (38.25%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
-    public int singleNonDuplicate(int[] arr) {
-         Arrays.sort(arr);
+    public int singleNonDuplicate(int[] arr ) {
+         
         int count = 0;
         for(int i = 0;i<arr.length;i++){
             count = count ^ arr[i];
