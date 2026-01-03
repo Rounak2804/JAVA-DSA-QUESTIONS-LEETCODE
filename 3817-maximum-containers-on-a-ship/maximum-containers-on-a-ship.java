9104617a9f8e8f93e88089d4500a9107f3bf8ec4
Rounak Raj <rounak00003@gmail.com>
2026-01-03 13:58:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (49.13%) - LeetSync

--- 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.java
+++ 3817-maximum-containers-on-a-ship/maximum-containers-on-a-ship.java
@@ -1,18 +1,12 @@
 class Solution {
     public int maxContainers(int n, int w, int maxWeight) {
-        int space = n*n;
-        int i = 1;
-        int container = 0;
-        while(i<=space){
-            
-            if(w*i<=maxWeight){
-                container++;
-            }
-            i++;
-
-        }
-        return container;
-        
-
+       int c = 0;
+       if(n*n*w<=maxWeight){
+        c = n*n;
+       }
+       else{
+        c = maxWeight/w;
+       }
+       return c;
     }
 }
