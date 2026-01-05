b1734ad89ed893d38cbf4c86db13f5b3929accc2
Rounak Raj <rounak00003@gmail.com>
2026-01-05 23:30:06 +0530

    Time: 1 ms (99.93%) | Memory: 46 MB (56.64%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] getSneakyNumbers(int[] arr) {
-        Arrays.sort(arr);
         int res[] = new int[2];
-        int a = 0;
-        for(int i = 0;i<arr.length;i++){
-            if(arr[i]==arr[i+1]){
-                res[a] = arr[i];
+         int a = 0;
+       for(int i = 0;i<arr.length;i++){
+        int curr= arr[i];
+        for(int j = i+1;j<arr.length;j++){
+            if(curr==arr[j]){
+                res[a] = curr;
                 a++;
             }
             if(a==2){
                 break;
             }
         }
-        return res;
+       }
+       return res;
     }
 }
