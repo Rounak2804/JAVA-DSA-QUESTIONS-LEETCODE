df6d6c16343155c6d915dd140d1875b52bcd7236
Rounak Raj <rounak00003@gmail.com>
2026-01-10 00:56:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (58.86%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.java
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -1,19 +1,15 @@
 class Solution {
     public int maximumWealth(int[][] arr) {
-        int max = Integer.MIN_VALUE;
-      
-        for (int i = 0; i < arr.length; i++) {
-               int result = 0;
-            for (int j = 0; j < arr[i].length; j++) {
-                result = result + arr[i][j];
+        int sum = 0;
+        for(int i = 0;i<arr.length;i++){
+                int c = 0;
+            for(int j = 0;j<arr[i].length;j++){
+                c += arr[i][j];
             }
-
-            if (max < result) {
-                max = result;
+            if(c>=sum){
+                sum = c;
             }
-
         }
-        return max;
-
+        return sum;
     }
 }
