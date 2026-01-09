af3018d8263fed23ba9eee63770b7b95b130083f
Rounak Raj <rounak00003@gmail.com>
2026-01-09 18:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.55%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -1,25 +1,10 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-        int o = num;
-        int r1 = 0;
-        while(o != 0){
-            int l = o % 10;
-            r1 = r1 * 10 + l;
-            o /= 10;
-        }
-        int r2 = 0;
-        int o1 = r1;
-        while(o1 != 0){
-            int l2 = o1 % 10;
-            r2 = r2 * 10+l2;
-            o1 /= 10;
-        }
-        if(r2==num){
-            return true;
-        }
-        else{
-            return false;
-        }
-
+       if(num % 10==0 && num != 0){
+        return false;
+       }
+       else{
+        return true;
+       }
     }
 }
