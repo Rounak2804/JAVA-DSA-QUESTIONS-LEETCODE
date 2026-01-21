321bd75ab015c0abee30b35666ce755fb2fc02a8
Rounak Raj <rounak00003@gmail.com>
2026-01-21 22:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (15.14%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        
+        int row = 0;
+        int col = arr[row].length - 1;
+
+        while(col>=0 && row<=arr.length -1 ) {
+            if(arr[row][col] == target){
+                return true;
+            }
+            else if(arr[row][col]<target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+        return false;
+    }
+}
