6d3687be298c04b1bba721fc3c2e9c8784994d32
Rounak Raj <rounak00003@gmail.com>
2026-01-28 21:42:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (47.43%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -2,21 +2,22 @@ class Solution {
     public int findMin(int[] arr) {
         // find pivot : get index of pivot thats the answer
         // small : arr[i-1] > arr[i] < arr[i+1]
-
-        int start = 0;
-        int end = arr.length - 1;
-
-        while (start < end) {
-            int mid = start + (end - start)/2;
-            
-        if(arr[mid]>arr[end]){
-            start = mid + 1;
-        }
-            else{
-               end = mid;   
+       int l = 0;
+       int r = arr.length - 1;
+       int min = 0;
+       while(l < r){
+                
+                int mid = l + (r - l) / 2;
+                if(arr[mid] < arr[r]){
+                    r = mid; 
+                    min = arr[mid];                      // minimum will be behind the mid
                 }
-        }
-
-        return arr[end];
+                else if(arr[mid]>arr[r]){
+                     l = mid + 1; 
+                            // min will be forward to mid
+                }
+       }
+       return arr[r];
+                
     }
 }
