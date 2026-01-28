52ecb1f5ec26e77fef0d42891b5f44511750a11b
Rounak Raj <rounak00003@gmail.com>
2026-01-28 23:12:51 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (75.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -5,13 +5,13 @@ class Solution {
 
         int min  = Smallest(arr);
         int f = First(arr , target , min);
-        
+        int s = Second(arr , target , min);
         
         if(target == arr[min]){
             return min;
         }
         else if(f==-1){
-            return Second(arr , target , min);
+            return s;
         }
         else{
             return f;
