eaa9819524d6a1e80693b6a594a0c8d8dee03ca0
Rounak Raj <rounak00003@gmail.com>
2026-02-01 14:36:40 +0530

    Time: 4 ms (17.06%) | Memory: 48.4 MB (54.73%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -2,16 +2,15 @@ class Solution {
     public int[] twoSum(int[] arr, int target) {
         int res[] = new int[2];
 
-        for(int i = 0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             int rem = target - arr[i];
-            int rem1 = Binary(arr , rem , i);
+            int rem1 = Binary(arr, rem, i);
 
-            if(arr[i] + arr[rem1]==target){
+            if (arr[i] + arr[rem1] == target) {
                 res[0] = i;
                 res[1] = rem1;
                 break;
             }
-
 
         }
         res[0] += 1;
@@ -21,24 +20,21 @@ class Solution {
 
     }
 
-    public int Binary(int arr[] , int rem , int i){
-        int s = i+1;
+    public int Binary(int arr[], int rem, int i) {
+        int s = i + 1;
         int e = arr.length - 1;
 
-        while(s<=e){
-            int mid = (s + e)/2;
-            if(arr[mid]==rem){
+        while (s < e) {
+            int mid = (s + e) / 2;
+            if (arr[mid] == rem) {
                 return mid;
-            }
-            else if(arr[mid]<rem){
+            } else if (arr[mid] < rem) {
                 s = mid + 1;
-            }
-            else{
+            } else {
                 e = mid - 1;
             }
         }
         return e;
     }
-
 
 }
