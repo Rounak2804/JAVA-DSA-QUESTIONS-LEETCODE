3fa544c1be1e2786a5b8bdd92196ad6501f96fb6
Rounak Raj <rounak00003@gmail.com>
2026-02-13 12:19:26 +0530

    Time: 2 ms (17.26%) | Memory: 43.6 MB (11.70%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
 
-         StringBuilder sb1 = new StringBuilder();
-                StringBuilder sb2 = new StringBuilder();
-                for(int i = 0;i< word1.length;i++){
-                    sb1.append(word1[i]);
-                }
-                for(int i = 0;i< word2.length;i++){
-                sb2.append(word2[i]);
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < word1.length; i++) {
+            sb1.append(word1[i]);
+        }
+        for (int i = 0; i < word2.length; i++) {
+            sb2.append(word2[i]);
         }
 
         String s1 = sb1.toString();
         String s2 = sb2.toString();
 
-        if(s1.length() != s2.length()){
+        if (s1.length() != s2.length()) {
             return false;
         }
 
-        for(int i = 0;i<s1.length();i++){
+        for (int i = 0; i < s1.length(); i++) {
             char a = s1.charAt(i);
             char b = s2.charAt(i);
-            if(a!=b){
+            if (a != b) {
                 return false;
             }
         }
